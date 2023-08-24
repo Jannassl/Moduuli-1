1b39608bcfcd9b79f59888bda4952d84d79faf01
Jannassl <Jan.nassling@metropolia.fi>
2023-08-24 14:22:05 +0300

    Tehtävä tehty

---
+++ Tehtävät/2 muuttujat ja vuorovaikutteiset ohjelmat/Tehtävä 4.py
@@ -0,0 +1,13 @@
+#summa, tulo ja keskiarvo
+
+luku1 = float(input("Anna 1. luku"))
+luku2 = float(input("Anna 2. luku"))
+luku3 = float(input("Anna 3. luku"))
+
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = (luku1 + luku2 + luku3) /3
+
+print(summa)
+print(tulo)
+print(keskiarvo)
