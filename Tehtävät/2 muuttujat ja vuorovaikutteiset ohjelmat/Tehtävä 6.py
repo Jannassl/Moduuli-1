d8cc56fb4b96f9a46b46f52a5c6b3d1db88dfdad
Jannassl <Jan.nassling@metropolia.fi>
2023-08-25 15:22:46 +0300

    Tehtävä tehty

--- Tehtävät/2 muuttujat ja vuorovaikutteiset ohjelmat/Tehtävä 6.py
+++ Tehtävät/2 muuttujat ja vuorovaikutteiset ohjelmat/Tehtävä 6.py
@@ -8,6 +8,6 @@ while i < 3:
     numero = random.randint(0,9)
     i +=1
     print(numero, end="")
-
+#Range laitetaan välille 1 - 6
 # Nelinumeroisen koodin saa kun: while i <= 3
 # Looppi käy itsensä läpi 4 kertaa
