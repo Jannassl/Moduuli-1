fd7cbd0b79f79e9805acd4a0feef4e1ba11d07a7
Jannassl <Jan.nassling@metropolia.fi>
2023-08-25 15:20:45 +0300

    Tehtävä tehty

---
+++ Tehtävät/2 muuttujat ja vuorovaikutteiset ohjelmat/Tehtävä 6.py
@@ -0,0 +1,13 @@
+import random
+
+
+i = 0
+
+while i < 3:
+
+    numero = random.randint(0,9)
+    i +=1
+    print(numero, end="")
+
+# Nelinumeroisen koodin saa kun: while i <= 3
+# Looppi käy itsensä läpi 4 kertaa
