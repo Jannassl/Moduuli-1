0091a7333d44c955eab5061d26045ba2a85c83d9
Jannassl <Jan.nassling@metropolia.fi>
2023-09-01 12:48:54 +0300

    Tehtävä tehty

---
+++ Tehtävät/6 Funktio/Tehtävä 6.1.py
@@ -0,0 +1,12 @@
+import random
+def silmaluku():
+
+
+    while True:
+        noppa = random.randint(1, 6)
+        if noppa !=6:
+            print(noppa)
+        else:
+            break
+
+silmaluku()
