7b88ad5f08b4084f4083149336e12150ecc41383
jannassling <Jannassl@metropolia.fi>
2023-10-23 13:49:06 +0300

    Tehtäviä tehty

---
+++ Tehtävät/10 Assosisaatio/Tehtävä 10.1.py
@@ -0,0 +1,33 @@
+class Hissi:
+    def __init__(self, alin_kerros, ylimm_kerros):
+        self.kerros = alin_kerros
+        self.alin_kerros = alin_kerros
+        self.ylin_kerros = ylimm_kerros
+
+    def siirry_kerrokseen(self, kohde_kerros):
+        if kohde_kerros < self.alin_kerros:
+            kohde_kerros = self.alin_kerros
+        elif kohde_kerros > self.ylin_kerros:
+            kohde_kerros = self.ylin_kerros
+
+        while self.kerros > kohde_kerros:
+            self.kerros_alas()
+        while self.kerros < kohde_kerros:
+            self.kerros_ylos()
+
+    def kerros_ylos(self):
+        if self.kerros < self.ylin_kerros:
+            self.kerros += 1
+        print(f'Hissi on kerroksessa {self.kerros}')
+
+    def kerros_alas(self):
+        if self.kerros > self.alin_kerros:
+            self.kerros -= 1
+        print(f'Hissi on kerroksessa {self.kerros}')
+
+
+# Testaus
+if __name__ == '__main__':
+    hissi = Hissi(alin_kerros=1, ylimm_kerros=10)
+    hissi.siirry_kerrokseen(5)
+    hissi.siirry_kerrokseen(1)
