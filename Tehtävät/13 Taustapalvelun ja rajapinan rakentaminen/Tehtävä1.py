7ecfc16cfe80eac7480d3e687a50f2f0b73454ab
Jannassl <Jan.nassling@metropolia.fi>
2023-11-14 13:39:26 +0200

    tehtäviä tehty

--- Tehtävät/13 Taustapalvelun ja rajapinan rakentaminen/Tehtävä1.py
+++ Tehtävät/13 Taustapalvelun ja rajapinan rakentaminen/Tehtävä1.py
@@ -1,22 +1,24 @@
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 @app.route('/alkuluku')
 
 def alkuluku():
     args = request.args
-    luku = int(args.get("number"))
+    luku = int(args.get("luku"))
+
     if luku % 2 == 0 or luku % 3 == 0:
-        print("Ei ole alkuluku")
-
+        vastaus = {"number": luku,
+                   "isPrime": False
+                   }
+        print(vastaus)
     else:
-        print("Luku on alkuluku")
-    vastaus = {
-        "number":luku
+        vastaus = {"number": luku,
+                   "isPrime": True
+                   }
+        print(vastaus)
 
-    }
-    return str(vastaus)
+if __name__=='__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
 
 
-
-if __name__=="__main__":
-    app.run(use_reloader=True,host='127.0.0.1',port=3000)
