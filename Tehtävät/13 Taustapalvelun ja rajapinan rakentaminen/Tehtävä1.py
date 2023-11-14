cf8ed3b1e0a6b9c32b618d2bbbfc61f7373ee8a8
Jannassl <Jan.nassling@metropolia.fi>
2023-11-14 19:22:00 +0200

    tehtävä 13 tehty

--- Tehtävät/13 Taustapalvelun ja rajapinan rakentaminen/Tehtävä1.py
+++ Tehtävät/13 Taustapalvelun ja rajapinan rakentaminen/Tehtävä1.py
@@ -1,22 +1,22 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-@app.route('/alkuluku')
+@app.route('/alkuluku/<luku>')
 
 def alkuluku():
     args = request.args
-    luku = int(args.get("luku"))
+    luku = float(args.get("luku"))
 
     if luku % 2 == 0 or luku % 3 == 0:
         vastaus = {"number": luku,
                    "isPrime": False
                    }
-        print(vastaus)
+        return str(vastaus)
     else:
         vastaus = {"number": luku,
                    "isPrime": True
                    }
-        print(vastaus)
+        return str(vastaus)
 
 if __name__=='__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
--- Tehtävät/13 Taustapalvelun ja rajapinan rakentaminen/Tehtävä2.py
+++ Tehtävät/13 Taustapalvelun ja rajapinan rakentaminen/Tehtävä2.py
@@ -1,13 +1,38 @@
+import mysql.connector
 from flask import Flask, request
-
 app = Flask(__name__)
-@app.route('/summa')
-def summa():
-    args = request.args
-    luku1 = float(args.get("luku1"))
-    luku2 = float(args.get("luku2"))
-    summa = luku1+luku2
-    return str(summa)
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='Nasslingmaga98',
+         autocommit=True
+         )
+@app.route('/kenttä/<icao>')
+def kenttä(icao):
+    sql = "SELECT airport.ident, airport.name, municipality FROM airport"
+    sql +=" INNER JOIN country ON country.iso_country = airport.iso_country"
+    sql +=" WHERE airport.ident='"+icao+"'"
+    arvot = []
+    sanakirja = {
+
+    }
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    i = 0
+    arvot = []
+    for rivi in tulos:
+        while i < 3:
+            arvot.append(rivi[i])
+            i+=1
+    #print(arvot)
+    sanakirja["ICAO"]= arvot[0]
+    sanakirja["Name"]= arvot[1]
+    sanakirja["Municipality"] = arvot[2]
+    return sanakirja
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
